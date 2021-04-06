df5caf765f8c26697127425c724e94e6381baf0b
Hori-Masayuki <hori.masayuki1132@gmail.com>
2021-04-07 01:30:59 +0900

    user_idをreqにset

--- src/servlet/Welcome.java
+++ src/servlet/Welcome.java
@@ -9,7 +9,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import bean.DataAccess;
 
@@ -33,9 +32,8 @@ public class Welcome extends HttpServlet {
 		// userCheckメソッドを実行し、画面遷移
 		try {
 			if (da.userCheck(name, password)) {
-				req.setAttribute("name", name);
-				HttpSession session = req.getSession();
-				session.setAttribute("id", da.getId());
+				req.setAttribute("user_id", da.getUser_id());
+				req.setAttribute("message", "ようこそ" + name + "さん");
 				RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/welcome.jsp");
 				rd.forward(req, resp);
 			} else {
@@ -44,8 +42,10 @@ public class Welcome extends HttpServlet {
 				rd.forward(req, resp);
 			}
 		} catch (SQLException e) {
-			// TODO 自動生成された catch ブロック
 			e.printStackTrace();
+			req.setAttribute("message", "エラーが発生しました");
+			RequestDispatcher rd = req.getRequestDispatcher("/index.jsp");
+			rd.forward(req, resp);
 		}
 	}
 }
